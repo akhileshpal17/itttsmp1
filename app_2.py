4558a0a3efe955d45b210a40644f388fc99ffc08
akhileshpal17 <92224454+akhileshpal17@users.noreply.github.com>
2021-11-12 12:25:57 +0530

    Update app_2.py

--- app_2.py
+++ app_2.py
@@ -8,7 +8,9 @@ import pytesseract
 import time
 import glob
 import os
-
+import base64 
+import time
+timestr = time.strftime("%Y%m%d-%H%M%S")
 
 from gtts import gTTS
 from googletrans import Translator
@@ -181,8 +183,25 @@ def main():
         st.write('Predicted Image')
         st.image(im, channels="BGR")
         st.write("Predicted Texts:",txt)
-   
 
+class FileDownloader(txt):
+	"""docstring for FileDownloader
+	download = FileDownloader(data,filename,file_ext).download()
+
+	"""
+	def __init__(self, data,filename='myfile',file_ext='txt'):
+		super(FileDownloader, self).__init__()
+		self.data = data
+		self.filename = filename
+		self.file_ext = file_ext
+
+	def download(self):
+		b64 = base64.b64encode(self.data.encode()).decode()
+		new_filename = "{}_{}_.{}".format(self.filename,timestr,self.file_ext)
+		st.markdown("#### Download File ###")
+		href = f'<a href="data:file/{self.file_ext};base64,{b64}" download="{new_filename}">Click Here!!</a>'
+		st.markdown(href,unsafe_allow_html=True)        
+ 
 
 
      
@@ -193,7 +212,9 @@ try:
     os.mkdir("temp")
 except:
     pass
-st.title("Text to speech")
+st.markdown("""
+    	#### Input text
+        """)
 translator = Translator()
 
 text = st.text_area("Enter text")
