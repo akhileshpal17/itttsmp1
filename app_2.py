b7b16c6d26bd43c26a4f2146dc6ebd0a7bcef84c
akhileshpal17 <92224454+akhileshpal17@users.noreply.github.com>
2021-11-12 12:30:21 +0530

    Update app_2.py

--- app_2.py
+++ app_2.py
@@ -184,23 +184,7 @@ def main():
         st.image(im, channels="BGR")
         st.write("Predicted Texts:",txt)
 
-class FileDownloader(txt):
-	"""docstring for FileDownloader
-	download = FileDownloader(data,filename,file_ext).download()
-
-	"""
-	def __init__(self, data,filename='myfile',file_ext='txt'):
-		super(FileDownloader, self).__init__()
-		self.data = data
-		self.filename = filename
-		self.file_ext = file_ext
-
-	def download(self):
-		b64 = base64.b64encode(self.data.encode()).decode()
-		new_filename = "{}_{}_.{}".format(self.filename,timestr,self.file_ext)
-		st.markdown("#### Download File ###")
-		href = f'<a href="data:file/{self.file_ext};base64,{b64}" download="{new_filename}">Click Here!!</a>'
-		st.markdown(href,unsafe_allow_html=True)        
+st.download_button(label='Download Text',data=txt,file_name='result.txt')     
  
 
 
